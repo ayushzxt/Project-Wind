0665a2a2be3cfa0271319333a35384bd59ccd4b5
AyushG-11 <ayushgupta9297@gmail.com>
2024-08-19 23:22:11 +0530

    httpclientfix

--- frontend/binance-frontend/app/utils/httpClient.ts
+++ frontend/binance-frontend/app/utils/httpClient.ts
@@ -9,14 +9,12 @@ export async function getTicker(market: string): Promise<Ticker> {
     if (!ticker) {
         throw new Error(`No ticker found for ${market}`);
     }
-    
     return ticker;
 }
-const x = getTickers()
 
-export async function getTickers(): Promise<number> {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    return 1;
+export async function getTickers(): Promise<Ticker[]> {
+    const response = await axios.get(`${BASE_URL}/tickers`);
+    return response.data;
 }
 
 
@@ -33,9 +31,4 @@ export async function getKlines(market: string, interval: string, startTime: num
     const response = await axios.get(`${BASE_URL}/klines?symbol=${market}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`);
     const data: KLine[] = response.data;
     return data.sort((x, y) => (Number(x.end) < Number(y.end) ? -1 : 1));
-}
-
-export async function getMarkets(): Promise<string[]> {
-    const response = await axios.get(`${BASE_URL}/markets`);
-    return response.data;
 }
